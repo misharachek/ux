81f1752eadc4ae0bf51d36d9e9f94706634c2d10
mikhailra <mikhail.rachek@sytoss.com>
2018-09-26 11:13:51 +0300

    creating todo service

--- angular2-todo-app/src/app/commons/todlist/services/todo.service.ts
+++ angular2-todo-app/src/app/commons/todlist/services/todo.service.ts
@@ -28,4 +28,33 @@ export class TodoService {
     return this;
   }
 
+  // update todo
+  public updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // get all todos
+  public  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // get todo/id
+  public getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // toggle todo compleate
+  toggleTodoComplete(todo: Todo) {
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
 }
