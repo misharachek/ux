12a2e9f09c48bc197c32935d9575461a6d0261c0
mikhailra <mikhail.rachek@sytoss.com>
2018-09-26 09:53:42 +0300

    creating todo service

--- angular2-todo-app/src/app/commons/todlist/services/todo.service.ts
+++ angular2-todo-app/src/app/commons/todlist/services/todo.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+// entity
+import {Todo} from '../entities';
 
 @Injectable()
 export class TodoService {
 
-  constructor() { }
+  // automatc incrementing of ids
+  lastId: number = 0;
+
+  // array todos
+  todos: Todo[] = [];
+
+  // post dodos
+  public addTodo(todo: Todo): TodoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  // delete todo
+  public deleteTodoById(id: number): TodoService {
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
 
 }
